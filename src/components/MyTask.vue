06b7c5c362c64944ba4b28421d9719b679047d9a
Marius Palade <marius.palade@whiteglove.com>
2025-08-09 17:25:55 +0300

    change task bg to red

--- src/components/MyTask.vue
+++ src/components/MyTask.vue
@@ -62,6 +62,7 @@ const pinTask = () => {
         :id="'title-' + task.id"
         name="title"
         placeholder="Input title"
+        style="background-color: red"
       />
     </label>
     <button
